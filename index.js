f1ba97f42d9d31a254ad540dfae77ac49329dc6e
mdavis9988 <58867354+mdavis9988@users.noreply.github.com>
2020-02-20 15:40:34 -0500

    added code to Dog app 2

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-'use strict';
+
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  fetch(`https://dog.ceo/api/breeds/image/random/${handleUserInput()}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+function handleUserInput() {
+  return $('.user-input').val(); 
+  
+  
+}
+
+
+function getImageIndex() {
+  let newArray= (${responseJson.message}).map  
+}
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  );
   //display the results section
   $('.results').removeClass('hidden');
 }
